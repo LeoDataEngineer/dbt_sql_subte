c333eb62323235b042192a98cfad410ccecb3edf
Leomar Estrada <leomarestradabsas@gmail.com>
2024-03-10 17:27:14 -0300

    primer copmmit

---
+++ proyecto_001/models/example/lineas_de_subte.sql
@@ -0,0 +1,19 @@
+
+
+{{ config(materialized='table') }}
+
+
+with data_cte as (
+
+   SELECT *
+   FROM SUBTEDATA
+
+)
+
+SELECT ROUTE_ID AS "LINEAS DE SUBTE",
+       COUNT(DISTINCT STOP_NAME) AS "CANTIDAD DE ESTACIONES",
+       CURRENT_TIMESTAMP() AS created_at
+FROM data_cte
+GROUP BY ROUTE_ID
+
+
